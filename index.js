b35d51415108510f1de6fa08f9b6b03bf77d0fc6
soninik33 <narutoandhinata2012@gmail.com>
2026-04-06 17:33:27 +0500

    obnova

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Telegraf } = require('telegraf');
 
-// Замените на ваш токен от @BotFather
 const bot = new Telegraf('8679972956:AAHDat96XDZHoLNU2q3uzmCFp1SwOom7N78');
 
 bot.start((ctx) => {
@@ -9,6 +8,27 @@ bot.start((ctx) => {
 
 bot.on('text', (ctx) => {
   ctx.reply('Ваше сообщение получено, ожидайте ответа врача.');
+});
+
+bot.hears('я хочу связаться с врачом', (ctx) => {
+    ctx.reply('Выберите нужного врача:', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: "Малика Алишеровна (Педиатр-невролог)", url: "https://t.me/MedPediatr_Bot" },
+                    { text: "Анвар Хакимов (Детский невролог)", url: "https://t.me/MedPediatrAbot" }
+                ],
+                [
+                    { text: "Лола Каримова (Детский ЛОР)", url: "https://t.me/MedPediatrLbot" },
+                    { text: "Санжар Юсупов (Детский офтальмолог)", url: "http://t.me/MedPediatrSbot" }
+                ],
+                [
+                    { text: "Наргиза Саидова (Педиатр-диетолог)", url: "http://t.me/MedPediatrNbot" },
+                    { text: "Дилшод Рахмонов (Детский хирург)", url: "http://t.me/MedPediatrRbot" }
+                ]
+            ]
+        }
+    });
 });
 
 bot.launch();
